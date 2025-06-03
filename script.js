7e7d22c4925e727b0e54c5180035f3191e90d7a7
Dawud-Shuaibu <dawudshuaibu6565@gmail.com>
2025-06-03 18:35:25 +0100

    version1

---
+++ script.js
@@ -0,0 +1,11 @@
+let count = document.getElementById('count');
+let increase_btn = document.getElementById('increase_btn');
+let decrease_btn = document.getElementById('decrease_btn');
+
+increase_btn.addEventListener('click', () => {
+    ++count.innerText;
+})
+
+decrease_btn.addEventListener('click', () => {
+    --count.innerText;
+})
