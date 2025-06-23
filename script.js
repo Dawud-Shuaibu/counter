bb3c7c44a6d19588c78f93fad8221636a8267ee4
Dawud-Shuaibu <dawudshuaibu6565@gmail.com>
2025-06-23 12:59:17 +0100

    version 1.1
    Updated the counter to be not decreasing when it reaches down to zero

--- script.js
+++ script.js
@@ -3,9 +3,10 @@ let increase_btn = document.getElementById('increase_btn');
 let decrease_btn = document.getElementById('decrease_btn');
 
 increase_btn.addEventListener('click', () => {
-    ++count.innerText;
+    ++count.innerText
 })
-
 decrease_btn.addEventListener('click', () => {
-    --count.innerText;
+    if (count.innerText > 0) {
+        --count.innerText
+    }
 })
